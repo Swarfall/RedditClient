ff6be6b98960e3a92d87d077ef11575f081256ea
Swarfall <savitsky.fit@gmail.com>
2021-01-29 22:27:31 +0200

    Add  UI to DetailVC

--- RedditClient/RedditClient/ViewControllers/Detail/DetailViewController.swift
+++ RedditClient/RedditClient/ViewControllers/Detail/DetailViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+final class DetailViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupView()
     }
     
 
+}
+
+// MARK: - Private
+private extension DetailViewController {
+    func setupView() {
+        
+    }
 }
